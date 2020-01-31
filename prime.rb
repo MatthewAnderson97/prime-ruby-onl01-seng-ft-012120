9aec6c478c962f77932426194a2c3f860e94b493
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-31 07:52:01 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,2 +1,12 @@
-
+def prime?(number)
+  i = 2
+  if number > 1
+    range = (i..number-1).to_a
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
+    end
+  else
+    false
+  end
+end
   #Add  code here!
